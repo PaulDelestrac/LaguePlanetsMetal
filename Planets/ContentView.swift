93ca425799f8cf2927601804cd2cbf23d325de9c
Paul Delestrac <paul.delestrac@icloud.com>
2024-12-06 22:22:30 +0100

    Initial Commit

---
+++ Planets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planets
+//
+//  Created by Paul Delestrac on 06/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Paul Delestrac on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
