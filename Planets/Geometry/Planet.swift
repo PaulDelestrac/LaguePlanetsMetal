9b074d97acf1474b5d51396928b4201e55a41b4d
Paul Delestrac <paul.delestrac@icloud.com>
2025-04-19 18:35:48 +0200

    Fixed lighting inconsistencies by merging terrain faces before recalculating normals; fixed code formatting

--- Planets/Geometry/Planet.swift
+++ Planets/Geometry/Planet.swift
@@ -8,90 +8,116 @@
 import Foundation
 import MetalKit
 
-class Planet : Transformable {
+class Planet: Transformable {
     let device: MTLDevice
     var transform = Transform()
-    
+
     var terrainFaces: [TerrainFace] = []
     var rawMesh: RawMesh = RawMesh(vertices: [], indices: [])
     var tiling: UInt32 = 1
-    
+
     var shapeSettings: ShapeSettings
     var shapeGenerator: ShapeGenerator
-    
+
     var colors: [SIMD4<Float>] = []
     var normals: [SIMD3<Float>] = []
-    
+
     var vertexBuffer: MTLBuffer!
     var indexBuffer: MTLBuffer!
     var colorBuffer: MTLBuffer!
     var normalBuffer: MTLBuffer!
-    
+
     let directions: [SIMD3<Float>] = [
         SIMD3<Float>(0, 1, 0),  // Top
-        SIMD3<Float>(0, -1, 0), // Bottom
-        SIMD3<Float>(-1, 0, 0), // Left
+        SIMD3<Float>(0, -1, 0),  // Bottom
+        SIMD3<Float>(-1, 0, 0),  // Left
         SIMD3<Float>(1, 0, 0),  // Right
         SIMD3<Float>(0, 0, 1),  // Front
-        SIMD3<Float>(0, 0, -1)  // Back
+        SIMD3<Float>(0, 0, -1),  // Back
     ]
-    
+
     enum FaceRenderMask: Int, Codable, Hashable {
-    case All, Top, Bottom, Left, Right, Front, Back
+        case All, Top, Bottom, Left, Right, Front, Back
     }
-    
+
     init(device: MTLDevice, shapeSettings: ShapeSettings, color: float3 = float3(0, 0, 0)) {
         self.device = device
         self.shapeSettings = shapeSettings
         self.shapeGenerator = ShapeGenerator(settings: self.shapeSettings)
         self.generateMesh()
-        
+
         // Initialize random colors
         for _ in self.rawMesh.vertices {
             // if no color is provided, set random colors
             if color == float3(0, 0, 0) {
-                self.colors.append(SIMD4<Float>(Float.random(in: 0...1), Float.random(in: 0...1), Float.random(in: 0...1), 1))
+                self.colors.append(
+                    SIMD4<Float>(
+                        Float.random(in: 0...1), Float.random(in: 0...1), Float.random(in: 0...1), 1
+                    ))
             } else {
                 self.colors.append(SIMD4<Float>(color, 1))
             }
         }
-        
+
         // Vertex buffer
-        guard let vertexBuffer = device.makeBuffer(bytes: &self.rawMesh.vertices, length: MemoryLayout<SIMD3<Float>>.stride * self.rawMesh.vertices.count, options: []) else {
+        guard
+            let vertexBuffer = device.makeBuffer(
+                bytes: &self.rawMesh.vertices,
+                length: MemoryLayout<SIMD3<Float>>.stride * self.rawMesh.vertices.count, options: []
+            )
+        else {
             fatalError("Unable to create planet vertex buffer")
         }
         self.vertexBuffer = vertexBuffer
-        
+
         // Index buffer
-        guard let indexBuffer = device.makeBuffer(bytes: &self.rawMesh.indices, length: MemoryLayout<UInt32>.stride * self.rawMesh.indices.count, options: []) else {
+        guard
+            let indexBuffer = device.makeBuffer(
+                bytes: &self.rawMesh.indices,
+                length: MemoryLayout<UInt32>.stride * self.rawMesh.indices.count, options: [])
+        else {
             fatalError("Unable to create planet index buffer")
         }
         self.indexBuffer = indexBuffer
-        
+
         // Color buffer
-        guard let colorBuffer = device.makeBuffer(bytes: &self.colors, length: MemoryLayout<SIMD4<Float>>.stride * self.colors.count, options: []) else {
+        guard
+            let colorBuffer = device.makeBuffer(
+                bytes: &self.colors, length: MemoryLayout<SIMD4<Float>>.stride * self.colors.count,
+                options: [])
+        else {
             fatalError("Unable to create planet color buffer")
         }
         self.colorBuffer = colorBuffer
-        
+
         // Normal buffer
-        guard let normalBuffer = device.makeBuffer(bytes: &self.normals, length: MemoryLayout<SIMD3<Float>>.stride * self.normals.count, options: []) else {
+        guard
+            let normalBuffer = device.makeBuffer(
+                bytes: &self.normals,
+                length: MemoryLayout<SIMD3<Float>>.stride * self.normals.count, options: [])
+        else {
             fatalError("Unable to create planet normal buffer")
         }
         self.normalBuffer = normalBuffer
     }
-    
+
     func generateMesh() {
         self.terrainFaces = []
 
         // Create the terrain faces
         for i in 0..<directions.count {
-            let renderFace: Bool = (shapeSettings.faceRenderMask == FaceRenderMask.All) || (shapeSettings.faceRenderMask.rawValue - 1 == i)
+            let renderFace: Bool =
+                (shapeSettings.faceRenderMask == FaceRenderMask.All)
+                || (shapeSettings.faceRenderMask.rawValue - 1 == i)
             if renderFace {
-                self.terrainFaces.append(TerrainFace(shapeGenerator: self.shapeGenerator, resolution: self.shapeSettings.resolution, localUp: directions[i]))
+                self.terrainFaces.append(
+                    TerrainFace(
+                        shapeGenerator: self.shapeGenerator,
+                        resolution: self.shapeSettings.resolution,
+                        localUp: directions[i]))
             }
         }
-        
+
         // Generate the mesh for each face
         self.rawMesh = RawMesh(vertices: [], indices: [], normals: [])
         for terrainFace in self.terrainFaces {
@@ -102,11 +128,15 @@ class Planet : Transformable {
             self.rawMesh.vertices.append(contentsOf: terrainFace.mesh.vertices)
             self.rawMesh.normals.append(contentsOf: terrainFace.mesh.normals)
         }
-        
+
         // Calculate normals
-        self.normals = self.rawMesh.normals
+        self.normals = calculateVertexNormals(
+            vertices: self.rawMesh.vertices, indices: self.rawMesh.indices)
+        self.rawMesh.normals = self.normals
+
+        mergeVerticesAndRecalculateNormals()
     }
-    
+
     func updateColor(_ color: SIMD3<Float>) {
         if colors.count != self.rawMesh.vertices.count {
             let newColors = Array(repeating: SIMD4<Float>(), count: self.rawMesh.vertices.count)
@@ -117,25 +147,70 @@ class Planet : Transformable {
             self.colors[colorIndex].y = color.y
             self.colors[colorIndex].z = color.z
         }
-        
-        self.colorBuffer = self.device.makeBuffer(bytes: &self.colors, length: MemoryLayout<SIMD4<Float>>.stride * self.colors.count, options: [])!
+
+        self.colorBuffer = self.device.makeBuffer(
+            bytes: &self.colors, length: MemoryLayout<SIMD4<Float>>.stride * self.colors.count,
+            options: [])!
     }
-    
+
     func updateShape(settings: ShapeSettings) {
         self.shapeSettings = settings
         self.shapeGenerator = ShapeGenerator(settings: settings)
         self.generateMesh()
-        self.vertexBuffer = self.device.makeBuffer(bytes: &self.rawMesh.vertices, length: MemoryLayout<SIMD3<Float>>.stride * self.rawMesh.vertices.count, options: [])!
-        self.indexBuffer = device.makeBuffer(bytes: &self.rawMesh.indices, length: MemoryLayout<UInt32>.stride * self.rawMesh.indices.count, options: [])!
-        self.normalBuffer = device.makeBuffer(bytes: &self.normals, length: MemoryLayout<SIMD3<Float>>.stride * self.normals.count, options: [])!
-        
+        self.vertexBuffer = self.device.makeBuffer(
+            bytes: &self.rawMesh.vertices,
+            length: MemoryLayout<SIMD3<Float>>.stride * self.rawMesh.vertices.count, options: [])!
+        self.indexBuffer = device.makeBuffer(
+            bytes: &self.rawMesh.indices,
+            length: MemoryLayout<UInt32>.stride * self.rawMesh.indices.count, options: [])!
+        self.normalBuffer = device.makeBuffer(
+            bytes: &self.normals, length: MemoryLayout<SIMD3<Float>>.stride * self.normals.count,
+            options: [])!
+
         self.updateColor(self.colors[0].xyz)
     }
-    
+
     func update(encoder: MTLRenderCommandEncoder) {
         encoder.setVertexBuffer(self.vertexBuffer, offset: 0, index: Position.index)
         encoder.setVertexBuffer(self.colorBuffer, offset: 0, index: Color.index)
         encoder.setVertexBuffer(self.normalBuffer, offset: 0, index: Normal.index)
+    }
+
+    func mergeVerticesAndRecalculateNormals() {
+        let epsilon: Float = 0.00001
+
+        var uniqueVertices: [SIMD3<Float>] = []
+        var newIndices: [UInt32] = []
+        var vertexMapping: [Int: Int] = [:]
+
+        for (index, vertex) in self.rawMesh.vertices.enumerated() {
+            var found = false
+            for (uniqueIndex, uniqueVertex) in uniqueVertices.enumerated() {
+                if distance(vertex, uniqueVertex) < epsilon {
+                    vertexMapping[index] = uniqueIndex
+                    found = true
+                    break
+                }
+            }
+
+            if !found {
+                vertexMapping[index] = uniqueVertices.count
+                uniqueVertices.append(vertex)
+            }
+        }
+
+        for oldIndex in self.rawMesh.indices {
+            if let newIndex = vertexMapping[Int(oldIndex)] {
+                newIndices.append(UInt32(newIndex))
+            }
+        }
+
+        self.rawMesh.vertices = uniqueVertices
+        self.rawMesh.indices = newIndices
+
+        self.normals = calculateVertexNormals(
+            vertices: self.rawMesh.vertices, indices: self.rawMesh.indices)
+        self.rawMesh.normals = self.normals
     }
 }
 
@@ -152,22 +227,23 @@ extension Planet {
         params.tiling = tiling
         uniforms.modelMatrix = transform.modelMatrix
         uniforms.normalMatrix = uniforms.modelMatrix.upperLeft
-        
+
         encoder.setVertexBytes(
             &uniforms,
             length: MemoryLayout<Uniforms>.stride,
             index: UniformsBuffer.index)
-        
+
         encoder.setFragmentBytes(
             &params,
             length: MemoryLayout<Params>.stride,
             index: ParamsBuffer.index)
-        
+
         self.update(encoder: encoder)
-        
+
         encoder.drawIndexedPrimitives(
-            type: .triangle, indexCount: self.rawMesh.indices.count, indexType: .uint32, indexBuffer: self.indexBuffer, indexBufferOffset: 0
-            )
+            type: .triangle, indexCount: self.rawMesh.indices.count, indexType: .uint32,
+            indexBuffer: self.indexBuffer, indexBufferOffset: 0
+        )
     }
 }
 
@@ -175,28 +251,28 @@ extension Planet {
 func calculateVertexNormals(vertices: [SIMD3<Float>], indices: [UInt32]) -> [SIMD3<Float>] {
     // Initialize normal vectors with zero
     var vertexNormals = [SIMD3<Float>](repeating: float3(0, 0, 0), count: vertices.count)
-    
+
     // Calculate face normals and accumulate for each vertex
     for i in stride(from: 0, to: indices.count, by: 3) {
         guard i + 2 < indices.count else { break }
-        
+
         let index0 = Int(indices[i])
         let index1 = Int(indices[i + 1])
         let index2 = Int(indices[i + 2])
-        
+
         let A = vertices[index0]
         let B = vertices[index1]
         let C = vertices[index2]
-        
+
         // Calculate face normal using cross product
         let faceNormal = cross(B - A, C - A)
-        
+
         // Accumulate normal for each vertex of the triangle
         vertexNormals[index0] += faceNormal
         vertexNormals[index1] += faceNormal
         vertexNormals[index2] += faceNormal
     }
-    
+
     // Normalize the accumulated normals
     return vertexNormals.map { vertex in
         return normalize(vertex)
--- Planets/Geometry/TerrainFace.swift
+++ Planets/Geometry/TerrainFace.swift
@@ -13,10 +13,10 @@ struct Vertex {
     var x: Float
     var y: Float
     var z: Float
-};
+}
 
 extension Vertex {
-    init (_ position: SIMD3<Float>) {
+    init(_ position: SIMD3<Float>) {
         self.x = position.x
         self.y = position.y
         self.z = position.z
@@ -31,44 +31,63 @@ struct RawMesh {
 
 class TerrainFace {
     var mesh = RawMesh(vertices: [], indices: [])
-    
+
     let shapeGenerator: ShapeGenerator
-    
+
     let resolution: Int
     let localUp: SIMD3<Float>
     var axisA: SIMD3<Float>
     var axisB: SIMD3<Float>
-    
-    init(shapeGenerator: ShapeGenerator, resolution: Int, localUp: SIMD3<Float>) {
+    // let flipNormals: Bool
+
+    init(
+        shapeGenerator: ShapeGenerator, resolution: Int, localUp: SIMD3<Float>,
+    ) {
         self.shapeGenerator = shapeGenerator
         self.resolution = resolution
         self.localUp = localUp
-        
+        // self.flipNormals = flipNormals
+
         self.axisA = SIMD3(x: localUp.y, y: localUp.z, z: localUp.x)
         self.axisB = SIMD3(x: localUp.z, y: localUp.x, z: localUp.y)
     }
-    
+
     func construct_mesh() {
         var vertices: [SIMD3<Float>] = []
         var triangles: [UInt32] = []
-        
+
         for y in 0..<self.resolution {
             for x in 0..<self.resolution {
                 let index = y * self.resolution + x
                 let percent = SIMD2<Float>(x: Float(x), y: Float(y)) / Float(self.resolution - 1)
-                let pointOnUnitCube = localUp + (Float(percent.x) - 0.5) * 2 * self.axisA + (Float(percent.y) - 0.5) * 2 * self.axisB
+                let pointOnUnitCube =
+                    localUp + (Float(percent.x) - 0.5) * 2 * self.axisA + (Float(percent.y) - 0.5)
+                    * 2 * self.axisB
                 let pointOnUnitSphere = normalize(pointOnUnitCube)
                 //vertices.append(Vertex(pointOnUnitCube))
-                vertices.append(shapeGenerator.calculatePointOnPlanet(pointOnUnitSphere: pointOnUnitSphere))
-                
+                vertices.append(
+                    shapeGenerator.calculatePointOnPlanet(pointOnUnitSphere: pointOnUnitSphere))
+
                 if (x != self.resolution - 1) && (y != self.resolution - 1) {
-                    triangles.append(UInt32(index))
-                    triangles.append(UInt32(index + self.resolution))
-                    triangles.append(UInt32(index + self.resolution + 1))
-                    
-                    triangles.append(UInt32(index))
-                    triangles.append(UInt32(index + self.resolution + 1))
-                    triangles.append(UInt32(index + 1))
+                    if (localUp.y > 0.9) || (localUp.z > 0.9)
+                        || (localUp.x > 0.9)
+                    {
+                        triangles.append(UInt32(index))
+                        triangles.append(UInt32(index + self.resolution))
+                        triangles.append(UInt32(index + self.resolution + 1))
+
+                        triangles.append(UInt32(index))
+                        triangles.append(UInt32(index + self.resolution + 1))
+                        triangles.append(UInt32(index + 1))
+                    } else {
+                        triangles.append(UInt32(index))
+                        triangles.append(UInt32(index + self.resolution + 1))
+                        triangles.append(UInt32(index + self.resolution))
+
+                        triangles.append(UInt32(index))
+                        triangles.append(UInt32(index + 1))
+                        triangles.append(UInt32(index + self.resolution + 1))
+                    }
                 }
             }
         }
@@ -76,10 +95,5 @@ class TerrainFace {
         self.mesh.vertices = vertices
         self.mesh.indices = triangles
         self.mesh.normals = calculateVertexNormals(vertices: vertices, indices: triangles)
-        if localUp.x < 0 || localUp.y < 0 || localUp.z < 0 {
-            self.mesh.normals = self.mesh.normals.map { normal in
-                return -normal
-            }
-        }
     }
 }
